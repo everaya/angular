80703f7b25e7d42e0d9501c0a2839d3c353530e8
eve <everaya95@gmail.com>
2020-03-19 13:19:48 -0400

    modified docs

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class= "app">
-      {{ title }}
+      <div>
+        <button (click) = "handleClick()" >
+          Change name
+        </button>
+        <input
+          type = "text"
+          [ngModel] = "name"
+          (ngModelChange) = "handleChange($event)"
+          >
+      </div>
+      <div> {{name}} </div>
     </div>
   `
 })
 export class AppComponent {
   title: string;
+  name: string = "Eve";
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = false;
+  logo: string = 'img/logo.svg';
+  handleChange(value : string){
+    this.name = value;
+  }
+
+  handleClick(event : any){
+    this.name = "Raya"
+  }
+
   constructor() {
     this.title = 'Ultimate course';
   }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common'
+import {FormsModule} from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +13,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    NgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//at run time, angular grabs NgModule, find imports
+//and bootstraps AppComponent (index.html)
