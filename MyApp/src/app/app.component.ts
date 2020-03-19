299a172e61482ae31409564622dec496bd7a6521
eve <everaya95@gmail.com>
2020-03-19 15:12:51 -0400

    trying two way binding

--- MyApp/src/app/app.component.ts
+++ MyApp/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   title = 'MyApp';
+  name: string = "Eve";
+  handleChange(value :string){
+    this.name = value
+  }
+  handleClick(){
+    this.name = "raya"
+  }
 }
